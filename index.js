cd24c4316363903ea96ca6eebc40cc2a9c7f407c
kameroongod <98730672+kameroongod@users.noreply.github.com>
2024-03-07 23:59:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -34,18 +34,20 @@ app.get("/blogs", (req, res) =>{
 });
 
 // Empty list to loop the blogs created with their associated images/text
-var imagesPath = [];
+var imagesPath = ["https://media.licdn.com/dms/image/C4D12AQE_9pYTyWLjUQ/article-cover_image-shrink_720_1280/0/1642563070109?e=2147483647&v=beta&t=wsjoV7WHU1-t9PKgifiRM5TaUQMkMVJAwt6Koa434_I",
+                 "https://s.yimg.com/ny/api/res/1.2/ZzvK1p0ca_uH2q.LAnz7IQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/coindesk_75/a46cc2d1876f4d619d9feb65cd6325d0",
+                 "https://cdn.sanity.io/images/cxgd3urn/production/02ecfb961c8a812f5d2ee15ab9fd62e02c71edad-960x480.jpg?w=1200&h=600&fit=crop&auto=format"];
 
 // Start carousel with images in folder once click discover
 app.post("/submit", (req, res) =>{
 
 // Retrieve list of the images URLs in images folder (Currently only 2 images)
 // Wanted to add option to add photo to blog (to-see)
-fs.readdir(__dirname + "/public/images/", (err, files) => {
-    files.forEach((file) => {
+//fs.readdir(__dirname + "/public/images/", (err, files) => {
+//    files.forEach((file) => {
         // Every static file has to be referenced from the public folder
-        imagesPath.push("/images/" + file)})
-});
+//        imagesPath.push("/images/" + file)})
+//});
 
 setTimeout(()=>{
   res.render("index.ejs", {imagesPath:imagesPath, blogsNames:blogsNames})},200)
